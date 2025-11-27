1a5d765f4c7e1a0ba94e004e11bd9fb17b359f5b
“farah-jannat” <farahjannatmithila@gmail.com>
2025-11-27 12:54:12 +0600

    chore: add shadcn

---
+++ frontend/src/constants.ts
@@ -0,0 +1,22 @@
+export const howItWorkSteps = [
+  {
+    heading: "Create a Job",
+    subheading:
+      "Sign up for free, set up your Job, and offer your work to our global audience.",
+  },
+  {
+    heading: "Deliver great work",
+    subheading:
+      "Get notified when you get an order and use our system to discuss details with customers.",
+  },
+  {
+    heading: "Get paid",
+    subheading:
+      "Get paid on time, every time. Payment is available for withdrawal as soon as it clears.",
+  },
+  {
+    heading: "Happy Client !",
+    subheading:
+      "Get paid on time, every time. Payment is available for withdrawal as soon as it clears.",
+  },
+];
